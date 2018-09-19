2dacc3b4d5ef10985202339a48248a28a4c789b7
Aditya Mangalampalli <Aditya_Mangalampalli@epam.com>
2018-09-19 19:54:36 +0530

    Section 3 - Project setup

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Chips","Some description","http://www.ricoh-imaging.co.jp/english/products/q-s1/ex/img/bod_mainImg_01.jpg"),
+    new Recipe("Roti","Ghee roti","http://7cc.ir/file/2/attach201501378501950614.jpg"),
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
